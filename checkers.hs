aed14dc31b7b41c5f73b97e735ac395d0b8741a0
Himnes <36138724+Himnes@users.noreply.github.com>
2018-02-09 15:20:38 +0100

    Update checkers.hs
    Board size is now 8 x 8

--- checkers.hs
+++ checkers.hs
@@ -1,13 +1,36 @@
 import Data.Char
 import Control.Monad
 import Control.Exception
+import System.Random
+import Debug.Trace
 
-data Square = Empty | White Position | Black Position
+data Square = Black | White deriving (Show,Eq)
+
+data Pcolor = PBlack | PWhite deriving (Show)
+data PType = Normal | Queen deriving (Show)
+data Piece = Piece Pcolor PType deriving (Show)
 
 type Board = [Square]
+
 type Position = (Int, Int)
+
 type Player = String
 
 type Move = (Int,Int)
+
+
+
+
+
+sizeofBoard = replicate 64 Black 
+
+makeBoard = let (x:xs) = sizeofBoard in
+                   makeBoardAux (x:xs) where
+                       makeBoardAux (x:y:xs) = Black:White:makeBoardAux xs
+                       makeBoardAux [] = []
+
+gameState = undefined
+
+movePlayer = undefined
 
 
